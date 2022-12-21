164c0b8e6ac4219a6992950b73b872eaa05545d4
Cherry Blossom <117899324+BrtnyE@users.noreply.github.com>
2022-12-21 15:17:16 +0800

    Update actionform.php

--- actionform.php
+++ actionform.php
@@ -1,22 +1,11 @@
 <?php
-/*
-This first bit sets the email address that you want the form to be submitted to.
-You will need to change this value to a valid email address that you can access.
-*/
+
 $webmaster_email = "britneyedok03@gmail.com";
 
-/*
-This bit sets the URLs of the supporting pages.
-If you change the names of any of the pages, you will need to change the values here.
-*/
 $feedback_page = "index.html";
 $error_page = "error_message.html";
 $thankyou_page = "thank_you.html";
 
-/*
-This next bit loads the form field data into variables.
-If you add a form field, you will need to add it here.
-*/
 $Name = $_REQUEST['name'] ;
 $Email = $_REQUEST['email_address'] ;
 $Subject = $_REQUEST['subject'] ;
@@ -27,10 +16,6 @@ $massage =
 "Subject: " . $Subject . "\r\n" .
 "Massage ;" . $Massage ;
 
-/*
-The following function checks for email injection.
-Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
-*/
 function isInjected($str) {
 	$injections = array('(\n+)',
 	'(\r+)',
@@ -50,21 +35,15 @@ function isInjected($str) {
 	}
 }
 
-// If the user tries to access this script directly, redirect them to the feedback form,
 if (!isset($_REQUEST['email_address'])) {
 header( "Location: $feedback_page" );
 }
 
-// If the form fields are empty, redirect to the error page.
-elseif (empty($first_name) || empty($email_address)) {
+elseif (empty($Name) || empty($Email)) {
 header( "Location: $error_page" );
 }
 
-/* 
-If email injection is detected, redirect to the error page.
-If you add a form field, you should add it here.
-*/
-elseif ( isInjected($email_address) || isInjected($first_name)  || isInjected($comments) ) {
+elseif ( isInjected($Name) || isInjected($Email)  || isInjected($Subject)   || isInjected($Massage) ) {
 header( "Location: $error_page" );
 }
 
